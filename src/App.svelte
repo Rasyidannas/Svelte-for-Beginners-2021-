0486de3c6e8d1ceac5820cf2671f99eec0c5bd41
rasyid annas <rasshit.dsgn@gmail.com>
2024-03-26 21:26:47 +0700

    4/3. Lesson 2 Conditional Rendering.mp4

--- src/App.svelte
+++ src/App.svelte
@@ -14,6 +14,18 @@
   <button class="btn-primary" on:click={add5}>Add Five</button>
 
   <p>The number multipled by 2 is {multiple}</p>
+
+  {#if multiple < 50}
+  <div role="alert" class="alert alert-success">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-info shrink-0 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+    <span>Numbers look good!</span>
+  </div>
+  {:else}
+  <div role="alert" class="alert alert-error">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-info shrink-0 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+    <span>Numbers are to high!</span>
+  </div>
+  {/if}
 </main>
 
 <style>
