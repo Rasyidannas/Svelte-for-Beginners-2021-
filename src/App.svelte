a6f656172ded89dd97d7f2d9ba0a057c9011ae1c
rasyid annas <rasshit.dsgn@gmail.com>
2024-03-26 21:12:36 +0700

    4/1. Lesson 0 Svelte Component Basics.mp4

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,14 @@
 <script>
-  
+  let count = 0;
+
+  function increment () {
+    count ++;
+  }
 </script>
 
-<main>
-
+<main class="flex flex-col justify-center items-center h-screen space-y-4">
+  {count}
+  <button on:click={increment} class="btn">Button</button>
 </main>
 
 <style>
