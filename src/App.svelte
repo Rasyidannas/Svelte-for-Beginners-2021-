d5733df32ac96c5074385cf95bebed735decdf9d
rasyid annas <rasshit.dsgn@gmail.com>
2024-03-26 22:29:39 +0700

    4/4. Lesson 3 Class Binding.mp4

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,26 @@
 <script>
-  let number = 1;
+  let active = 1;
 
-  function add5() {
-    number += 5;
+  function changeActive(num) {
+    active = num;
   }
-
-  // This is a reactive variable
-  $: multiple = number * 2;
 </script>
 
 <main class="flex flex-col justify-center items-center h-screen space-y-4">
-  <p>The number is: {number}</p>
-  <button class="btn-primary" on:click={add5}>Add Five</button>
-
-  <p>The number multipled by 2 is {multiple}</p>
-
-  {#if multiple < 50}
-  <div role="alert" class="alert alert-success">
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-info shrink-0 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-    <span>Numbers look good!</span>
+  <div class="join">
+    <button 
+      on:click={() => changeActive(1)} 
+      class="btn join-item {active === 1 ? "btn-active" : ""}"
+      >Button</button>
+    <button 
+      on:click={() => changeActive(2)} 
+      class="btn join-item {active === 2 ? "btn-active" : ""}"
+      >Button</button>
+    <button 
+      on:click={() => changeActive(3)} 
+      class="btn join-item {active === 3 ? "btn-active" : ""}"
+      >Button</button>
   </div>
-  {:else}
-  <div role="alert" class="alert alert-error">
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-info shrink-0 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-    <span>Numbers are to high!</span>
-  </div>
-  {/if}
 </main>
 
 <style>
